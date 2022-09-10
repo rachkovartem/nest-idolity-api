dab79a1134e4e50ea4a0eafdedfdfe954df8163b
Artem Rachkov <a.rachkov94@gmail.com>
2022-09-10 09:19:30 +0300

    remove cors from main

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders:
-      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
-  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
