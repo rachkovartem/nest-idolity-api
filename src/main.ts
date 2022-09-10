a708a4420451eb17c08952d3ee7445ac4fdeb38c
Artem Rachkov <a.rachkov94@gmail.com>
2022-09-10 15:51:11 +0300

    add preflight at main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    exposedHeaders: '',
+    credentials: true,
+    preflightContinue: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
